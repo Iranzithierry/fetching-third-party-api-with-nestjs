1b068363d6dcf0264b9bfaa673f4a5cf1a7f07b2
Iranzi Dev <iranzithierry12@gmail.com>
2024-03-07 19:49:05 +0200

    Update products.controller.ts

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,30 +6,16 @@ import { Product } from 'src/interfaces/product';
 @Controller('products')
 export class ProductsController {
     constructor(private productService: ProductsService) { }
-    /**
-     * 
-     * @param searchQuery
-     * @returns 
-    */
     @Get('/')
     @ApiQuery({ name: 'search', required: false })
     async getProducts(@Query('search') searchQuery?: string): Promise<any> {
         return this.productService.getProducts(searchQuery);
     }
-    /**
-     * 
-     * @param id 
-     * @returns {Promise<any>}
-     */
     @Get('/:id')
     async getProductById(@Param('id') id: string): Promise<any> {
         return this.productService.getProductById(id);
     }
-    /**
-     * 
-     * @param body 
-     * @returns 
-     */
+    // 
     @Post('/')
     async createProduct(@Body() body: Product): Promise<any> {
         return this.productService.createProduct(body);
